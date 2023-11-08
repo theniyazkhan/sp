01d3b809f66c97067d5209176aebe4c2105fcd54
Niyaz Ahmad Khan <ahmadkhanniyaz@gmail.com>
2023-11-08 18:47:46 +0600

    Add files via upload

---
+++ reverseArray.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size){
+    int i = 0;
+    int j = size - 1;
+    while (i < j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+    for (int i=0; i<n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
